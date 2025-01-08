a85cfcc07f5c60e380caf96f7f9c4dbe161f5229
belvi <pouadjeubelvi@gmail.com>
2025-01-08 10:31:36 +0100

    Adding new todo

--- src/main/java/com/belvi/management_todo/controller/TodoController.java
+++ src/main/java/com/belvi/management_todo/controller/TodoController.java
@@ -3,6 +3,8 @@ package com.belvi.management_todo.controller;
 import com.belvi.management_todo.model.Todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,11 +12,20 @@ import java.util.List;
 
 @RestController
 public class TodoController {
+    // List of todos
     private List<Todo> todos = new ArrayList<>();
 
+    // Get all todos
     @GetMapping("api/todos")
     public List<Todo> getAllTodos(){
 
         return todos;
+    }
+
+    // Add new todo
+    @PostMapping("api/todos")
+    public String addTodo(@RequestBody Todo todo){
+        todos.add(todo);
+        return "Todo added sucessfully";
     }
 }
